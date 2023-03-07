5532702cec95ec4bc86c07d0e0b8fa1f7381c5a8
Ethanol <119572743+Swoiksdmop@users.noreply.github.com>
2023-03-07 16:56:23 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,37 @@
+print("|ˉˉˉ|     |ˉˉˉ|    |ˉˉˉ|")
+print("|   |     |   |    |   |")
+print("|   |     |   |    |   |")
+print("|   |_____|   |    |   |")
+print("|             |    |   |")
+print("|   |ˉˉˉˉˉ|   |    |   |")
+print("|   |     |   |    |   |")
+print("|   |     |   |    |   |")
+print("|   |     |   |    |   |")
+print("|   |     |   |    |   |")
+print("|___|     |___|    |___|")
+
+
+
+list_of_names = ('Ethan', 'Audrey', 'Benjamin', 'Dylan')
+
+print(list_of_names)
+
+name = input("What is your name? >")
+
+print("Welcome " + name)
+
+age = input("How old are you? >")
+
+store_age = (age)
+
+print("You are now " + store_age)
+
+evil = input("Are you evil? >")
+
+if evil == "no":
+    print("thanks for not being honest with me")
+
+else:
+    print("no")
+
+
